ae020855f58806f472cf6ebf628076b6a8924db6
Jjeter <Jacobmjeter@gmail.com>
2014-12-02 11:46:48 -0600

    Added Initial Code

---
+++ Elevator Code.c
@@ -0,0 +1,22 @@
+
+/*
+  Project Title:Vex Elevator
+  Team Members:Jacob Jeter, Cade Fleaher, Allison Mcgehee, Kenzie Yarbrough
+  Date:12/2/14
+  Section:
+
+
+  Task Description:
+
+
+  Pseudocode:
+
+*/
+
+task main()
+{                                     //Program begins, insert code within curly braces
+
+
+
+
+}
